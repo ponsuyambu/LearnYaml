e94c25137c2ba34994f51ab35965f04ea89bc13f
Pons <pons.velladurai@omio.com>
2019-09-17 17:48:51 +0200

    feat: test

--- app/src/main/java/de/ponshere/learnyaml/MainActivity.kt
+++ app/src/main/java/de/ponshere/learnyaml/MainActivity.kt
@@ -36,7 +36,7 @@ class MainActivity : AppCompatActivity() {
         //tes
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
-        //patch in master branch
+        //patch in master branchj
         //feature in master branch
         //feature3 breaking change
     }
