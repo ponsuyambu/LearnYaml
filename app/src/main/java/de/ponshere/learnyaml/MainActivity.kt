6bf0737845140fb3c9d42446741e1403f7ebe991
Pons <pons.velladurai@omio.com>
2019-09-17 16:20:33 +0200

    [bump-major]: Major release

--- app/src/main/java/de/ponshere/learnyaml/MainActivity.kt
+++ app/src/main/java/de/ponshere/learnyaml/MainActivity.kt
@@ -37,7 +37,7 @@ class MainActivity : AppCompatActivity() {
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
         //patch in master branch
-        //feature in master branch
+        //feature in master branchl
         //feature3 breaking change
     }
 }
