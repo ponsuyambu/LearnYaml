997136bff8746c8743f9cf5fc1a0ea833a8a59cb
Pons <pons.velladurai@omio.com>
2019-09-17 11:20:16 +0200

    BREAKING CHANGE: another one

--- app/src/main/java/de/ponshere/learnyaml/MainActivity.kt
+++ app/src/main/java/de/ponshere/learnyaml/MainActivity.kt
@@ -22,6 +22,7 @@ class MainActivity : AppCompatActivity() {
         //Patch fix
         //feature
         //Breaking Change
+        //Another Breaking Change
         //patch again
         val appBarConfiguration = AppBarConfiguration(
             setOf(
